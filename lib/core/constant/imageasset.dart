c64db67b598e9488f6486a09a987dd405e9f6b9f
KingNarmar <adlymina99@gmail.com>
2023-06-02 10:10:42 +0300

    add imageAsset constant

---
+++ lib/core/constant/imageasset.dart
@@ -0,0 +1,7 @@
+class ImageAsset {
+  static const String rootImages = "assets/images";
+  static const String onBoardingImageOne = "$rootImages/onboardingone.PNG";
+  static const String onBoardingImageTow = "$rootImages/onboardingtwo.PNG";
+  static const String onBoardingImageThree = "$rootImages/onboardingthree.PNG";
+  static const String onBoardingImageFour = "$rootImages/onboardingfour.PNG";
+}
