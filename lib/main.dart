8aada161e605e464aeaa7e255222d2b788d25800
KingNarmar <adlymina99@gmail.com>
2023-06-02 09:42:58 +0300

    add assets images,fonts and create onBoarding Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce/view/screen/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return   GetMaterialApp(
+      title:  "E Commerce App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const OnBoarding(),
+    );
   }
 }
